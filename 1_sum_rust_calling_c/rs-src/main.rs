eba391a2629f7941386a9e8874df1a2b9b2222d2
Vincent Pun <vpun@ymail.com>
2022-07-28 21:05:20 -0700

    Makefile improvements

--- 1_sum_rust_calling_c/rs-src/main.rs
+++ 1_sum_rust_calling_c/rs-src/main.rs
@@ -5,6 +5,8 @@ extern "C" {
 }
 
 fn main() {
+    // This must be unsafe because there's no way for Rust to know anything. You are responsible for
+    // ensuring safety.
     let result = unsafe { sum(5, 3) };
     println!("Result: {result}");
 }
